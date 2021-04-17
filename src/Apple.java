dc68a67288d09b1626ba5ed64c3767f037a3546a
Daniil <daniilrabcenkov@gmail.com>
2021-04-17 17:15:18 +0300

    coздал проект

---
+++ src/Apple.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Apple {
+	int x, y;
+	Color color;
+	int SIZE;
+	Random rn;
+
+	Apple() {
+		rn = new Random();
+		x = rn.nextInt(10);
+		y = rn.nextInt(10); /* rn.nextInt(); */
+		SIZE = -1 * (rn.nextInt(3) + 1);
+		color = new Color(rn.nextInt(256), rn.nextInt(256), rn.nextInt(256));
+	}
+}
---
+++ src/Head.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class Head {
+	int x, y;
+	Color color;
+
+	public Head() {
+		x = y = 5;
+		color = Color.red;
+
+	}
+
+	public boolean checkOutOfBounds() {
+		if (x < 0 || x > 9 || y < 0 || y > 9) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
